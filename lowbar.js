dba0bb0f3c70ec4072b5fdfec8e4712bea0576df
two-triangles <danielsalehguitar@gmail.com>
2021-05-13 12:49:14 +0100

    complete up to and inc map function

--- lowbar.js
+++ lowbar.js
@@ -1,7 +1,35 @@
-const _ = {};
+const _ = {}
 
 _.identity = (x) => {
   return x;
 };
 
+
+_.fromPairs = (arrPairs) => {
+  if(!arrPairs.length) return {}
+  const returnObj = {}
+
+  for (let i = 0; i < arrPairs.length; i++) {
+    returnObj[arrPairs[i][0]] = arrPairs[i][1]
+  }
+  return returnObj
+}
+
+ 
+
+_.map = (collection, func) => {
+  let input = collection
+  if (!Array.isArray(collection)) {
+    const values = Object.values(collection)
+    input = values
+  }
+   for (let i = 0; i < input.length; i++) {
+     func(input[i])
+   }
+  return input
+}
+
 module.exports = _;
+
+// const secondNestedElement = arrPairs[0][1]
+// const firstNestedElement = arrPairs[0][0]
