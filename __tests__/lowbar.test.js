8cbf59a3cac7ab9b4b7c534b4072379b3e40848b
DCrawley94 <duncan.crawley@gmail.com>
2021-07-01 16:45:56 +0100

    filter func working for arrays

--- __tests__/lowbar.test.js
+++ __tests__/lowbar.test.js
@@ -1,79 +1,126 @@
 const _ = require('../lowbar');
 
 describe('#identity', () => {
-  test('returns the value passed as an argument', () => {
-    expect(_.identity(3)).toBe(3);
-    expect(_.identity('hello')).toBe('hello');
-    expect(_.identity(true)).toBe(true);
-  });
-  test('returns a reference to the same reference type passed as an argument', () => {
-    const arr = [];
-    expect(_.identity(arr)).toEqual(arr);
-  });
+	test('returns the value passed as an argument', () => {
+		expect(_.identity(3)).toBe(3);
+		expect(_.identity('hello')).toBe('hello');
+		expect(_.identity(true)).toBe(true);
+	});
+	test('returns a reference to the same reference type passed as an argument', () => {
+		const arr = [];
+		expect(_.identity(arr)).toEqual(arr);
+	});
 });
 
 describe('#fromPairs', () => {
-  test('returns empty obj if passed empty array', () => {
-    expect(_.fromPairs([])).toEqual({})
-  });
-  test('return same number of key/val pairs as nested arrays args provided', () => {
-    let outPut = _.fromPairs([["a", 1]])
-    expect(Object.keys(outPut).length).toBe(1)
+	test('returns empty obj if passed empty array', () => {
+		expect(_.fromPairs([])).toEqual({});
+	});
+	test('return same number of key/val pairs as nested arrays args provided', () => {
+		let outPut = _.fromPairs([['a', 1]]);
+		expect(Object.keys(outPut).length).toBe(1);
 
-    outPut = _.fromPairs([["a", 1], ["b", 2]])
-    expect(Object.keys(outPut).length).toBe(2)
-  });
-  test('returns obj with a single key/value pair', () => {
-    expect(_.fromPairs([['a', 1]])).toEqual({a: 1})
-  });
-  test('returns obj with multiple key/val pairs when given multiple nested arrays', () => {
-    expect(_.fromPairs([['a', 1], ["b", 2]])).toEqual({a: 1, b: 2})
-    expect(_.fromPairs([['a', 1], ["b", 2], ["c", 3], ["d", 4]])).toEqual({a: 1, b: 2, c:3, d: 4})
-  });
-  test('does not mutate original array input', () => {
-    const input = [["a", 1]]
-    _.fromPairs(input)
-    expect(input).toEqual([["a", 1]])
-  });
+		outPut = _.fromPairs([
+			['a', 1],
+			['b', 2]
+		]);
+		expect(Object.keys(outPut).length).toBe(2);
+	});
+	test('returns obj with a single key/value pair', () => {
+		expect(_.fromPairs([['a', 1]])).toEqual({ a: 1 });
+	});
+	test('returns obj with multiple key/val pairs when given multiple nested arrays', () => {
+		expect(
+			_.fromPairs([
+				['a', 1],
+				['b', 2]
+			])
+		).toEqual({ a: 1, b: 2 });
+		expect(
+			_.fromPairs([
+				['a', 1],
+				['b', 2],
+				['c', 3],
+				['d', 4]
+			])
+		).toEqual({ a: 1, b: 2, c: 3, d: 4 });
+	});
+	test('does not mutate original array input', () => {
+		const input = [['a', 1]];
+		_.fromPairs(input);
+		expect(input).toEqual([['a', 1]]);
+	});
 });
 
-
 describe('#map', () => {
-  test('if passed empty array or object - returns empty array', () => {
-    expect(_.map([])).toEqual([])
-    expect(_.map({})).toEqual([])
-  });
-  test('return array length is the same as either - array arg length or number of key/value pairs on obj arg', () => {
-    const mockFunc = jest.fn()
-    const inputArr = [1, 2]
-    const inputObj = {"a": 1, "b": 2}
-    expect(_.map(inputArr, mockFunc)).toHaveLength(2)
-    expect(_.map(inputObj, mockFunc)).toHaveLength(2)
-  });
-  test('given function is called on each iteratee if passed an array', () => {
-    const mockFunc = jest.fn()
-    const inputArr = [1, 2]
-   
-    _.map(inputArr, mockFunc)
-    expect(mockFunc).toHaveBeenCalledTimes(2)
-  });
-  test('given function is called on each value of a passed object', () => {
-    const mockFunc = jest.fn()
-    const inputObj = {"a": 1, "b": 2}
-    
-   _.map(inputObj, mockFunc)
-    expect(mockFunc).toHaveBeenCalledTimes(2)
-  });
-  test('should not mutate given collection', () => {
-    const mockFunc = jest.fn()
-    let input = [1, 2]
-    _.map(input, mockFunc)
-    expect(input).toEqual([1, 2])
+	test('if passed empty array or object - returns empty array', () => {
+		expect(_.map([])).toEqual([]);
+		expect(_.map({})).toEqual([]);
+	});
+	test('return array length is the same as either - array arg length or number of key/value pairs on obj arg', () => {
+		const mockFunc = jest.fn();
+		const inputArr = [1, 2];
+		const inputObj = { a: 1, b: 2 };
+		expect(_.map(inputArr, mockFunc)).toHaveLength(2);
+		expect(_.map(inputObj, mockFunc)).toHaveLength(2);
+	});
+	test('given function is called on each iteratee if passed an array', () => {
+		const mockFunc = jest.fn();
+		const inputArr = [1, 2];
 
-    input = {"a": 1, "b": 2}
-    _.map(input, mockFunc)
-    expect(input).toEqual({"a": 1, "b": 2})
-  });
+		_.map(inputArr, mockFunc);
+		expect(mockFunc).toHaveBeenCalledTimes(2);
+	});
+	test('given function is called on each value of a passed object', () => {
+		const mockFunc = jest.fn();
+		const inputObj = { a: 1, b: 2 };
 
+		_.map(inputObj, mockFunc);
+		expect(mockFunc).toHaveBeenCalledTimes(2);
+	});
+	test('should not mutate given collection', () => {
+		const mockFunc = jest.fn();
+		let input = [1, 2];
+		_.map(input, mockFunc);
+		expect(input).toEqual([1, 2]);
 
+		input = { a: 1, b: 2 };
+		_.map(input, mockFunc);
+		expect(input).toEqual({ a: 1, b: 2 });
+	});
+});
+
+describe.only('#filter', () => {
+	test('if passed empty array or object - returns empty array', () => {
+		expect(_.filter({})).toEqual([]);
+		expect(_.filter([])).toEqual([]);
+	});
+	describe('-- Arrays --', () => {
+		test('should return array of length 1 - predicate return true', () => {
+			const testPred = (num) => !(num % 2);
+			const testArr = [2];
+			expect(_.filter(testArr, testPred)).toEqual([2]);
+		});
+		test('should return array of length 0 - predicate return false', () => {
+			const testPred = (num) => !(num % 2);
+			const testArr = [1];
+			expect(_.filter(testArr, testPred)).toEqual([]);
+		});
+		test('predicate should be called on each array element', () => {
+			const mockFn = jest.fn();
+			const testArr = [1, 2, 3, 4, 5];
+			_.filter(testArr, mockFn);
+			expect(mockFn).toHaveBeenCalledTimes(5);
+			expect(mockFn).toHaveBeenCalledWith(1);
+			expect(mockFn).toHaveBeenCalledWith(2);
+			expect(mockFn).toHaveBeenCalledWith(3);
+			expect(mockFn).toHaveBeenCalledWith(4);
+			expect(mockFn).toHaveBeenCalledWith(5);
+		});
+		test('should correctly filter longer arrays', () => {
+			const mockFn = jest.fn().mockImplementation((num) => !(num % 2));
+			const testArr = [1, 2, 3, 4, 5];
+			expect(_.filter(testArr, mockFn)).toEqual([2, 4]);
+		});
+	});
 });
--- lowbar.js
+++ lowbar.js
@@ -1,35 +1,39 @@
-const _ = {}
+const _ = {};
 
 _.identity = (x) => {
-  return x;
+	return x;
 };
 
-
 _.fromPairs = (arrPairs) => {
-  if(!arrPairs.length) return {}
-  const returnObj = {}
+	if (!arrPairs.length) return {};
+	const returnObj = {};
 
-  for (let i = 0; i < arrPairs.length; i++) {
-    returnObj[arrPairs[i][0]] = arrPairs[i][1]
-  }
-  return returnObj
-}
-
- 
+	for (let i = 0; i < arrPairs.length; i++) {
+		returnObj[arrPairs[i][0]] = arrPairs[i][1];
+	}
+	return returnObj;
+};
 
 _.map = (collection, func) => {
-  let input = collection
-  if (!Array.isArray(collection)) {
-    const values = Object.values(collection)
-    input = values
-  }
-   for (let i = 0; i < input.length; i++) {
-     func(input[i])
-   }
-  return input
-}
+	let input = collection;
+	if (!Array.isArray(collection)) {
+		const values = Object.values(collection);
+		input = values;
+	}
+	for (let i = 0; i < input.length; i++) {
+		func(input[i]);
+	}
+	return input;
+};
 
+_.filter = (collection, predicate) => {
+	const filteredArr = [];
+	for (let i = 0; i < collection.length; i++) {
+		if (predicate(collection[i])) filteredArr.push(collection[i]);
+	}
+	return filteredArr;
+};
+//takes array or object
+//iterates and calls predicate func on each item
+//return new array
 module.exports = _;
-
-// const secondNestedElement = arrPairs[0][1]
-// const firstNestedElement = arrPairs[0][0]
